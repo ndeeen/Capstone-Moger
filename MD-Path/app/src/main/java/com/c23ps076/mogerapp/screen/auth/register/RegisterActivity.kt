d3fc4ea33e6fbc724bc6b5b16e7aaa8e4e5610fa
fairofif <fairofif@gmail.com>
2023-06-08 20:31:51 +0700

    registerUI ready implemented

---
+++ MD-Path/app/src/main/java/com/c23ps076/mogerapp/screen/auth/register/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package com.c23ps076.mogerapp.screen.auth.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
