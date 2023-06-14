d25479121cc69167d2ca577a1a7728b3db3a8f99
fairofif <fairofif@gmail.com>
2023-06-15 06:44:53 +0700

    memberManagementUI Ready Implemented

---
+++ MD-Path/app/src/main/java/com/c23ps076/mogerapp/screen/MemberManagementActivity.kt
@@ -0,0 +1,11 @@
+package com.c23ps076.mogerapp.screen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MemberManagementActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_member_management)
+    }
+}
