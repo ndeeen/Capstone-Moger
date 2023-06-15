4c80f94a8a40e9fbb3ee445fed10bdc6d5d2eb4f
fairofif <fairofif@gmail.com>
2023-06-15 10:29:55 +0700

    addTransactionUI ready implement

---
+++ MD-Path/app/src/main/java/com/c23ps076/mogerapp/screen/addTransaction/AddTransactionActivity.kt
@@ -0,0 +1,11 @@
+package com.c23ps076.mogerapp.screen.addTransaction
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddTransactionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_transaction)
+    }
+}
