0681f4f4f54579e758f5ae253e9bd4855f10cefb
fairofif <fairofif@gmail.com>
2023-06-15 15:01:17 +0700

    walletManagement UI ready implement

---
+++ MD-Path/app/src/main/java/com/c23ps076/mogerapp/screen/walletManagement/WalletManagementActivity.kt
@@ -0,0 +1,11 @@
+package com.c23ps076.mogerapp.screen.walletManagement
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WalletManagementActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wallet_management)
+    }
+}
